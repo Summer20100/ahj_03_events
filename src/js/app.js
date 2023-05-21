4596b6ec85238a41c5273f0a84eb66eaf9aaea7f
Dmitrii Bulatov <bulati484@gmail.com>
2023-05-21 20:18:38 +0300

    finish

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-import '../css/style.css';
-import GoblinGame from './GoblinGame';
-
-window.addEventListener('load', () => {
-  const goblinGame = new GoblinGame();
-  goblinGame.boardGeneration(4);
-  goblinGame.shootsRecorder();
-  goblinGame.imgCreate(1000);
-});
