f3d11f843a3f097131626dfdb4b474fffe228852
Dmitrii Bulatov <bulati484@gmail.com>
2023-06-01 22:35:17 +0300

    some corrections

--- src/js/GoblinGame.js
+++ src/js/GoblinGame.js
@@ -1,28 +1,33 @@
 import image from '../assets/goblin.png';
 
 export default class GoblinGame {
-  constructor() {
+  constructor(boarSize, interval) {
     this.position = 0;
     this.field = 0;
-    this.boarSize = 0;
+    this.boarSize = boarSize
     this.successShot = 0;
     this.bossShot = 0;
     this.rounds = 0;
     this.gameResults = document.createElement('p');
+    this.interval = interval;
+
+    this.imageGoblin = new Image();
+    this.imageGoblin.src = image;
+    this.imageGoblin.classList.add('red-head');
   }
 
   goblinSetter() {
     return Math.floor(Math.random() * (this.boarSize ** 2));
   }
 
-  boardGeneration(countOfCells) {
-    this.boarSize = countOfCells;
+  boardGeneration() {
+    this.boarSize = this.boarSize;
     this.field = document.createElement('div');
     this.field.classList.add('board');
-    const width = 124 * countOfCells;
+    const width = 124 * this.boarSize;
     this.field.style.width = `${width}px`;
     document.body.appendChild(this.field);
-    for (let i = 0; i < countOfCells ** 2; i += 1) {
+    for (let i = 0; i < this.boarSize ** 2; i += 1) {
       const cell = document.createElement('div');
       cell.classList.add('cell');
       this.field.appendChild(cell);
@@ -30,40 +35,40 @@ export default class GoblinGame {
     document.body.appendChild(this.gameResults);
   }
 
-  imgCreate(interval) {
-    const goblin = new Image();
-    goblin.src = image;
-    goblin.classList.add('red-head');
-    let intervalId = setInterval(() => {
+  setInterval(interval) {
+    const intervalId  = setInterval(() => {
       if (this.bossShot > 4) {
         clearInterval(intervalId);
-        intervalId = null;
-        document.body.insertAdjacentHTML(
-          'beforeEnd',
+        document.body.insertAdjacentHTML('beforeEnd',
           `<div class="modal_mask">
             <div class="modal">
-              <div class="modal_msg">Ты был не точен, игра окончена.</div>
+              <div class="modal_msg">В следующий раз обязятельно получится</div>
               <button class="close_btn">Закрыть</button>
             </div>
           </div>`,
         );
-        const clsModalBtn = document.querySelector('.close_btn');
-        clsModalBtn.addEventListener('click', (ev) => ev.target.closest('div.modal_mask').remove() & location. reload());
+        const closeBtn = document.querySelector('.close_btn');
+        closeBtn.addEventListener('click', (ev) => {
+          ev.target.closest('div.modal_mask').remove();
+        })
       }
+
       const position = this.goblinSetter();
-      this.field.childNodes[position].appendChild(goblin);
+      this.field.childNodes[position].appendChild(this.imageGoblin);
       this.rounds += 1;
-      this.gameResults.textContent = `Попадания: ${this.successShot} / Промахи: ${this.bossShot} / Число попыток: ${this.rounds}`;
-    }, interval);
+      this.gameResults.textContent = `Число поподаний: ${this.successShot} / Число промахов: ${this.bossShot} / Всего раз перебежал гоблин: ${this.rounds}`
+    }, interval)
   }
 
   shootsRecorder() {
+    this.setInterval(this.interval);
     document.body.addEventListener('click', (ev) => {
       if (ev.target.classList.contains('red-head')) {
         this.successShot += 1;
-        ev.target.closest('img.red-head').remove()
+        this.setInterval(this.interval);
       } else if (ev.target.classList.contains('cell')) {
         this.bossShot += 1;
+        this.setInterval(this.interval);
       }
     });
   }
--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,8 @@ import '../css/style.css';
 import GoblinGame from './GoblinGame';
 
 window.addEventListener('load', () => {
-  const goblinGame = new GoblinGame();
-  goblinGame.boardGeneration(3);
+  const goblinGame = new GoblinGame(3, 1000);
+  goblinGame.boardGeneration();
   goblinGame.shootsRecorder();
-  goblinGame.imgCreate(1000);
+  //goblinGame.imgCreate(1000);
 });
